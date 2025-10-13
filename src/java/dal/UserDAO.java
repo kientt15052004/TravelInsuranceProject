ca7bcc31c7c19253451cd2a89d55e40163837c4c
DuongDDTHE171411 <duongddthe171411@fpt.edu.vn>
2025-10-13 14:43:26 +0700

    Update insert User

--- src/java/dal/UserDAO.java
+++ src/java/dal/UserDAO.java
@@ -58,28 +58,7 @@ public class UserDAO extends DBContext {
     }
     return false;
 }
-
-public void insertUser(User u) {
-    String sql = "INSERT INTO users(username, password, fullname, mail, dob, address, phone, role, status) "
-               + "VALUES(?,?,?,?,?,?,?,?,?)";
-    try {
-        PreparedStatement st = connection.prepareStatement(sql);
-        st.setString(1, u.getUsername());
-        st.setString(2, u.getPassword());
-        st.setString(3, u.getFullname());
-        st.setString(4, u.getMail());
-        st.setDate(5, java.sql.Date.valueOf(u.getDob()));
-        st.setString(6, u.getAddress());
-        st.setString(7, u.getPhone());
-        st.setString(8, u.getRole());
-        st.setString(9, u.getStatus());
-        st.executeUpdate();
-    } catch (SQLException e) {
-        e.printStackTrace();
-    }
-}
-
-    
+ 
     // Get user by CCCD number
     public User getUserByCccd(String cccd) {
         String sql = "SELECT * FROM users WHERE cccd = ?";
